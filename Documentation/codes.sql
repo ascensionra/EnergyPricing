14cd5c0f9cc970ac4c0ad1e1e2fd8fbdc92d0b9f
Jared McArthur <jared.mcarthur@utexas.edu>
2015-03-19 23:09:04 -0500

    INSERT tweaks

--- Documentation/codes.sql
+++ Documentation/codes.sql
@@ -1,3 +1,10 @@
-INSERT INTO PET_SERIES (SERIES_ID,DESCRIPTION,UM) VALUES ([PET_MCRSFP31_A],[Gulf Coast (PADD 3) Crude Oil Stocks at Tank Farms, Annual],[Thousand Barrels
-])INSERT INTO PET_SERIES (SERIES_ID,DESCRIPTION,UM) VALUES ([PET_MCRSFP31_A],[Gulf Coast (PADD 3) Crude Oil Stocks at Tank Farms, Annual],[Thousand Barrels
-])
+INSERT INTO PET_SERIES VALUES (q'[PET_MCRSFP31_A]',q'[Gulf Coast (PADD 3) Crude Oil Stocks at Tank Farms, Annual]',q'[Thousand Barrels
+]');
+INSERT INTO PET_SERIES VALUES (q'[PET_MCRST_YCUOK_1_M]',q'[Cushing, OK Ending Stocks of Crude Oil, Monthly]',q'[Thousand Barrels
+]');
+INSERT INTO PET_SERIES VALUES (q'[PET_MCRSFP51_M]',q'[West Coast (PADD 5) Crude Oil Stocks at Tank Farms, Monthly]',q'[Thousand Barrels
+]');
+INSERT INTO PET_SERIES VALUES (q'[PET_MCRSFUS1_M]',q'[U.S. Crude Oil Stocks at Tank Farms, Monthly]',q'[Thousand Barrels
+]');
+INSERT INTO PET_SERIES VALUES (q'[PET_MCRSFP11_A]',q'[East Coast (PADD 1) Crude Oil Stocks at Tank Farms, Annual]',q'[Thousand Barrels
+]');
