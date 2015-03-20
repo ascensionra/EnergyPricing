f76aa266c522510b4710f44b284b1a91d16fe467
Jared McArthur <jared.mcarthur@utexas.edu>
2015-03-19 22:40:43 -0500

    added codes.sql for testing inserts to Oracle DB

---
+++ Documentation/codes.sql
@@ -0,0 +1,3 @@
+INSERT INTO PET_SERIES (SERIES_ID,DESCRIPTION,UM) VALUES (PET_MCRSFP31_A,Gulf Coast (PADD 3) Crude Oil Stocks at Tank Farms, Annual,Thousand Barrels
+)INSERT INTO PET_SERIES (SERIES_ID,DESCRIPTION,UM) VALUES (PET_MCRSFP31_A,Gulf Coast (PADD 3) Crude Oil Stocks at Tank Farms, Annual,Thousand Barrels
+)
