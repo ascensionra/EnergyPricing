9d579ba116806d94c9897bda90008c1cbc3d7b36
Jared McArthur <jared.mcarthur@utexas.edu>
2015-03-26 00:17:17 -0500

    a few changes to restful.py

--- scripts/Restful.py
+++ scripts/Restful.py
@@ -1,11 +1,14 @@
 import urllib,urllib2,httplib
 
-url = 'http://http://129.152.144.84:5001/rest/native/?'
+url = 'http://129.152.144.84:5001/rest/native/?'
 
-parameters = urllib.urlencode({ 'query':'INSERT INTO PET_SERIES(NAME,DESCRIPTION,SERIES_ID) VALUES ("a","b","c")','verbose':'TRUE' })
+parameters = { 'query':'INSERT INTO PET_SERIES(NAME,DESCRIPTION,SERIES_ID) VALUES (\"a\",\"b\",\"c\")','verbose':'TRUE' }
 
-headers = {'DB':jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal','USER':'cs370_jsm3287','PASS':'orcl','MODE':'native_mode','MODEL':'model','returnDimensions':'False','returnFor':'JSON','d':'i[0]','v':'i[1]' }
+headers = {'DB':'jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal','USER':'cs370_jsm3287','PASS':'orcl','MODE':'native_mode','MODEL':'model','returnDimensions':'False','returnFor':'JSON','d':'i[0]','v':'i[1]' }
 
 bigurl = url + urllib.urlencode(parameters) + urllib.urlencode(headers)
+print bigurl
+
 
 resp = urllib.urlopen(bigurl)
+print resp.getcode()
