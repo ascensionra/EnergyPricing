574156d479fe9bdc0bf828857c2e90f20de9d73e
Jared McArthur <jared.mcarthur@utexas.edu>
2015-04-07 23:10:11 -0500

    minor updates to Restful.py and .gitignore'

--- scripts/Restful.py
+++ scripts/Restful.py
@@ -1,8 +1,8 @@
 import urllib,urllib2,httplib
 
-url = 'http://129.152.144.84:5001/rest/native/?'
+url = 'http://129.152.144.84:5001/rest/native/'
 
-parameters = { 'query':'INSERT INTO PET_SERIES(NAME,DESCRIPTION,SERIES_ID) VALUES (\"a\",\"b\",\"c\")','verbose':'TRUE' }
+parameters = { '?query':'"INSERT INTO PET_SERIES(NAME,DESCRIPTION,SERIES_ID) VALUES (\"a\",\"b\",\"c\")"','verbose':'TRUE' }
 
 headers = {'DB':'jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal','USER':'cs370_jsm3287','PASS':'orcl','MODE':'native_mode','MODEL':'model','returnDimensions':'False','returnFor':'JSON','d':'i[0]','v':'i[1]' }
 
