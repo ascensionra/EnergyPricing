4ceab942ea86e0f51251112bca8593a3b6daeaf5
Jared McArthur <jared.mcarthur@utexas.edu>
2015-04-07 23:08:50 -0500

    added new module for accessing EIA

---
+++ Module/eia.py
@@ -0,0 +1,9 @@
+""" This module is for using the EIA API for accessing
+data series """
+import requests, json, sys
+
+token = None
+url = None
+
+if __name__ == '__main__':
+	print "Please don't try to run me"
