1dc98277cc978159dc78e8e5d6a70268d02f25dd
Jared McArthur <jared.mcarthur@utexas.edu>
2015-05-04 01:04:59 -0500

    added createTable

--- Module/eia.py
+++ Module/eia.py
@@ -2,8 +2,35 @@
 data series """
 import requests, json, sys
 
-def getData(*args):
-	print args
+def getSeriesData(ser,apiKey):
+	""" Retrieves series data specified by 'ser'
+and returns the standard EIA JSON object """
+	url = 'http://api.eia.gov/series/?api_key=' + apiKey + '&series_id=' + ser.upper()
+	try:
+	 return requests.get(url).json()
+	except HTTPError as e:
+		print('HTTP error type.')
+		print('Error code: ', e.code)
+
+	except URLError as e:
+		print('URL type error.')
+		print('Reason: ', e.reason)
+
+def createTable(series_id,header):
+	""" Creates a table if it does not already exits in
+the Oracle database. Due to current state of RESTful
+API, we cannot return a success code. Header with connection
+information will need to be provided. See example. """
+
+	""" Example header for connecting to Oracle DB 
+	head = {'DB':'jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',\
+			'USER':'username','PASS':'password','MODE':'native_mode','MODEL':'model','returnDimensions':'False','returnFor':'JSON' }
+	"""
+	series_id = "\"" + series_id + "\""
+	url = 'http://129.152.144.84:5001/rest/native/?query=\'CREATE TABLE IF NOT EXISTS ' + series_id + \
+			' (PRICE_DATE DATE NOT NULL, PRICE NUMBER)\''
+	print 'Attempting to create table ' + series_id + '\nUsing url ' + url
+	return requests.get(url,headers=header)
 
 def printJson(response):
   """ Prints JSON dictionary of a successful
@@ -27,4 +54,5 @@ response in a nice format to a file """
   return
 
 if __name__ == '__main__':
-	print "Please don't try to run me"
+	#print "Please don't try to run me"
+	exit
