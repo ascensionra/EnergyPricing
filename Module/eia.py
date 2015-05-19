0ae1e0c9a6958db88fd3dfa02aee239c11338dd3
Jared McArthur <jared.mcarthur@utexas.edu>
2015-05-18 23:35:15 -0500

    getAlias and createTAble are fully functional

--- Module/eia.py
+++ Module/eia.py
@@ -1,7 +1,7 @@
 """ This module is for using the EIA API for accessing
 data series """
 #TODO: fix module so it can be deployed in a working Python environment
-import requests, json, sys
+import requests, json, sys, re
 
 glbUrl = 'http://129.152.144.84:5001/rest/native/?query='
 glbCount = 0
@@ -34,6 +34,8 @@ API, we cannot return a success code. Header with connection
 information will need to be provided. See example. """
 # TODO: check if table exists, first. Putting it in the 
 #		query itself seems to break the execution.
+#		May not be necessary, since the work in done server-side, and will fail
+#		silently is table already exists.
 
 	""" Example header for connecting to Oracle DB 
 	head = {'DB':'jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',\
@@ -42,40 +44,38 @@ information will need to be provided. See example. """
 	"""
 
 	global glbUrl
-	alias = getAlias(series_id,header)
-	series_alias = '\"' + alias + '\"'
-	url = glbUrl + '\'CREATE TABLE ' + series_alias + ' (PRICE_DATE DATE NOT NULL, PRICE NUMBER (12,3) )\'' 
-	print 'Attempting to create table ' + series_alias + '\nUsing url ' + url
+	global glbCount
+	print '*****************************************************************'
+	alias = getAlias(series_id,header)  
+	url = glbUrl + '\'CREATE TABLE ' + alias + ' (PRICE_DATE DATE NOT NULL, PRICE NUMBER (12,3) )\'' 
+	print 'Attempting to create table ' + alias + '\nUsing url ' + url
+	glbCount += 1 # this is for debugging to detect name collisions
+
 	try:
 		return requests.get(url,headers=header)
 	except requests.exceptions.RequestException,e:
 		print 'The request failed: %s' % (e)
-	except HTTPError as e:
-		print('HTTP error type.')
-		print('Error code: ', e.code)
-	except URLError as e:
-		print('URL type error.')
-		print('Reason: ', e.reason)
 
 ##############################################################################
 def getAlias(series_id,header):
-	""" Returns the alias for the series_id from the ALIASES table """
+	""" Returns the alias for the series_id from the ALIASES table
+for the createTable method. Eliminated need for double-quoting table name
+and keeps names (nominally) under 30 bytes """
 
 	global glbUrl
-	series_id = '\"' + series_id + '\"'
-	qry = '\'SELECT ALIAS FROM ALIASES WHERE NAME = ' + series_id + '\''
+	qry = '\"SELECT * FROM ALIASES WHERE NAME = \'' + series_id + '\'\"'
 	url = glbUrl + qry
 	print 'Trying to get alias for ' + series_id + '\nUsing url ' + url
+
 	try:	
-		printJson(requests.get(url,headers=header))	
+		s = json.loads(re.sub(',\]',']',requests.get(url,headers=header).text))
+#		printDict(s,'k')
+		return str(s['ALIAS'][0])
 	except requests.exceptions.RequestException,e:
 		print 'The request failed: %s' % (e)
-	except HTTPError as e:
-		print('HTTP error type.')
-		print('Error code: ', e.code)
-	except URLError as e:
-		print('URL type error.')
 		print('Reason: ', e.reason)
+	except BaseException, e:
+		print 'Unexpected error: %s' % (e)
 
 ##############################################################################
 def dropTable(series_id,header):
@@ -89,19 +89,37 @@ Header with connection information will need to be provided. See example. """
 	"""
 
 	global glbUrl
-	series_id = '\"' + series_id + '\"'
-	url = glbUrl + '\'DROP TABLE ' + series_id + '\'' 
-	print 'Attempting to drop table ' + series_id + '\nUsing url ' + url
+	alias = getAlias(series_id, header)
+	url = glbUrl + '\"DROP TABLE ' + alias + '\"' 
+	print 'Attempting to drop table ' + alias + '\nUsing url ' + url
 	try:
 		return requests.get(url,headers=header)
 	except requests.exceptions.RequestException,e:
 		print 'The request failed: %s' % (e)
-	except HTTPError as e:
-		print('HTTP error type.')
-		print('Error code: ', e.code)
-	except URLError as e:
-		print('URL type error.')
-		print('Reason: ', e.reason)
+	except BaseException, e:
+		print 'Unknown exception: %s' % (e)
+
+##############################################################################
+def truncateTable(series_id,header):
+	""" Truncates the specified table from the Oracle database. 
+Due to current state of RESTful API, we cannot return a success code. 
+Header with connection information will need to be provided. See example. """
+
+	""" Example header for connecting to Oracle DB 
+	head = {'DB':'jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',\
+			'USER':'username','PASS':'password','MODE':'native_mode','MODEL':'model','returnDimensions':'False','returnFor':'JSON' }
+	"""
+
+	global glbUrl
+	alias = getAlias(series_id, header)
+	url = glbUrl + '\'TRUNCATE TABLE ' + alias + '\'' 
+	print 'Attempting to drop table ' + alias + '\nUsing url ' + url
+	try:
+		return requests.get(url,headers=header)
+	except requests.exceptions.RequestException,e:
+		print 'The request failed: %s' % (e)
+	except BaseException, e:
+		print 'Unknown exception: %s' % (e)
 
 ##############################################################################
 def insertRecords(seriesJson,h):
@@ -139,12 +157,6 @@ Must supply headers as in createTable """
 		requests.get(url2,headers=h)	
 	except requests.exceptions.RequestException,e:
 		print 'The request failed: %s' % (e)
-	except HTTPError as e:
-		print('HTTP error type.')
-		print('Error code: ', e.code)
-	except URLError as e:
-		print('URL type error.')
-		print('Reason: ', e.reason)
 	except BaseException,e:
 		print e
 		return
@@ -173,6 +185,21 @@ response in a nice format to a file """
   except BaseException,e:
     print 'Undexpected error: %s' % (e)
   return
+
+##############################################################################
+def printDict(dictionary,sortVal):
+	""" Prints out the contents of a dictionary in a nice format
+using OrderedDict from collections to sort the dict.""" 
+	from collections import OrderedDict as OD
+	try:
+		if (sortVal.upper() == 'K' or sortVal.upper() == 'KEY'):
+			ordered = OD(sorted(dictionary.items(), key = lambda t: t[0]))
+		else:
+			ordered = OD(sorted(dictionary.items(), key = lambda t: t[1], reverse=True))
+		for key in ordered:
+			print '%s: %s' % (key,ordered[key])
+	except BaseException,e:
+		print 'Unexpected error: %s' % (e)
 
 ##############################################################################
 if __name__ == '__main__':
